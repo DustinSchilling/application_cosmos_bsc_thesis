6d42d10b6fa633b720fe4864a36940fb3378bee4
Dustin Schilling <82500269+DustinSchilling@users.noreply.github.com>
2022-07-11 17:14:17 +0200

    scripts added

---
+++ scripts/preparation_sample_analysis.R
@@ -0,0 +1,114 @@
+library(readr)
+library(readxl)
+library(cosmosR)
+library(decoupleR)
+library(SummarizedExperiment)
+library(dplyr)
+library(tidyr)
+library(tibble)
+
+## Data is already processed -> log2() transformation
+
+raw_metab <- readRDS("data/SummarizedExperiment_extraction_method_cohort_metabolomics.RDS")
+raw_prot <- readRDS("data/SummarizedExperiment_extraction_method_cohort_proteomics.RDS")
+
+
+# annotation tumor and healthy tissue
+# raw_metab
+metab_count <- as.data.frame(raw_metab@assays@data@listData[[1]])
+colnames(metab_count) <- raw_metab@colData@listData[["Sample.Description"]]
+
+metab_count <- unique(metab_count)
+metabolomics_TUvsNG <- as.data.frame(metab_count)
+
+
+# raw_prot
+prot_count <- as.data.frame(raw_prot@assays@data@listData[[1]])
+colnames(prot_count) <- raw_prot@colData@listData[["Pseudonym"]]
+prot_count <- tibble::rownames_to_column(prot_count, "ID")
+
+
+prot_count <- prot_count %>%
+  mutate(ID = strsplit(as.character(ID), ";")) %>%
+  unnest(ID) %>%
+  filter(ID != "")
+
+prot_count <- prot_count[prot_count$ID != "NA",]
+prot_count <- unique(prot_count)
+
+proteomics_TUvsNG <- as.data.frame(prot_count[,-1])
+rownames(proteomics_TUvsNG) <- prot_count$ID
+
+# additionally differentiate between autoSP3 and MTBE
+autoSP3orMTBE <- raw_prot@colData@listData[["Processing"]]
+sum(autoSP3orMTBE == "MTBE_SP3")
+# first 18 are MTBE_SP3, autoSP3 are only 17
+# some samples do not have healthy or cancerous partner with sample preparation
+# Thereby, these will be combined so it ends up with 20 samples that are pair-wise combined just like in metabolomics 
+# We will do these manually so it is exactly the same as metabolomics
+proteomics_TUvsNG <- proteomics_TUvsNG[,c(6, 5, 12, 11, 16, 15, 14, 13, 2, 1, 27, 26, 24, 23, 18, 17, 9, 8, 4, 3)]
+# "IJ17TV_NG"  "IJ17TV_TU" "8U04FR_NG"  "8U04FR_TU" these samples are from autoSP3 and not MTBE_SP3
+colnames(proteomics_TUvsNG) <- gsub("[.]1", "", colnames(proteomics_TUvsNG))
+colnames(proteomics_TUvsNG)[1] <- "8JDLQY_TU"
+
+# save raw matrices
+saveRDS(metabolomics_TUvsNG, "./results/metabolomics_TUvsNG.Rda")
+saveRDS(proteomics_TUvsNG, "./results/proteomics_TUvsNG.Rda")
+
+## computation for z-transformation
+# log-FC
+
+# metab
+metabolomics_NG <- metabolomics_TUvsNG[,seq(2, 20, by = 2)]
+metabolomics_TU <- metabolomics_TUvsNG[,seq(1, 20, by = 2)]
+logFC_metabolomics_TUvsNG <- metabolomics_TU - metabolomics_NG
+colnames(logFC_metabolomics_TUvsNG) <- gsub("_TU", "", colnames(logFC_metabolomics_TUvsNG))
+
+
+rowMeans(logFC_metabolomics_TUvsNG[which(rownames(logFC_metabolomics_TUvsNG) == "Putrescine"),])
+
+
+
+# prot
+proteomics_NG <- proteomics_TUvsNG[,seq(2, 20, by = 2)]
+proteomics_TU <- proteomics_TUvsNG[,seq(1, 20, by = 2)]
+logFC_proteomics_TUvsNG <- proteomics_TU - proteomics_NG
+colnames(logFC_proteomics_TUvsNG) <- gsub("_TU", "", colnames(logFC_proteomics_TUvsNG))
+
+rowMeans(logFC_proteomics_TUvsNG[which(rownames(logFC_proteomics_TUvsNG) == "LAMA5"),], na.rm =T)
+
+## z-transformation
+#metab
+mean_metabolomics_FC <- rowMeans(as.matrix(logFC_metabolomics_TUvsNG), na.rm = T)
+sd_metabolomics_FC <- rowSds(as.matrix(logFC_metabolomics_TUvsNG), na.rm = T)
+logFC_metabolomics_z <- as.data.frame((logFC_metabolomics_TUvsNG - mean_metabolomics_FC)/sd_metabolomics_FC)
+
+#prot
+mean_proteomics_FC <- rowMeans(as.matrix(logFC_proteomics_TUvsNG), na.rm = T)
+sd_proteomics_FC <- rowSds(as.matrix(logFC_proteomics_TUvsNG), na.rm = T)
+logFC_proteomics_z <- as.data.frame((logFC_proteomics_TUvsNG - mean_proteomics_FC)/sd_proteomics_FC)
+
+# save
+saveRDS(logFC_metabolomics_z, "./results/logFC_metabolomics_z.Rda")
+saveRDS(logFC_proteomics_z, "./results/logFC_proteomics_z.Rda")
+
+
+
+
+
+
+### adjust metab_file
+metabolomics_DE_t <- as.data.frame(read_delim("data/metabolomics_DE_t_TUvsNG.txt", 
+                                              delim = "\t", escape_double = FALSE, 
+                                              trim_ws = TRUE))
+metabolomics_DE_t$logFC<- metabolomics_DE_t$logFC*-1
+metabolomics_DE_t$t<- metabolomics_DE_t$t*-1
+
+write_csv(metabolomics_DE_t, "C:/Users/DustinSchilling/Documents/GitHub/prot_met_workflow/data/metabolomics_adjusted.csv")
+
+
+
+# compare to before 
+metab_old <- as.data.frame(read_delim("data/metabolomics_DE_t_TUvsNG.txt", 
+                                              delim = "\t", escape_double = FALSE, 
+                                              trim_ws = TRUE))
---
+++ scripts/run_cosmos.R
@@ -0,0 +1,131 @@
+library(readr)
+library(readxl)
+library(cosmosR)
+library(decoupleR)
+
+data("meta_network")
+meta_network <- meta_network[which(meta_network$source != meta_network$target),]
+
+metabolomics_DE_t <- read_csv("data/metabolomics_adjusted.csv")
+proteomics_DE_t <- as.data.frame(read_delim("data/proteomics_DE_t_TUvsNG_autoSP3.txt", 
+                                            delim = "\t", escape_double = FALSE, 
+                                            trim_ws = TRUE))
+
+Biocrates_metabolite_identifier <- read_excel("support/Biocrates_metabolite_identifier.xlsx")
+Biocrates_metabolite_identifier$feature <- gsub("[()/: -]",".",Biocrates_metabolite_identifier$feature)
+
+metabolomics_DE_t <- merge(metabolomics_DE_t, Biocrates_metabolite_identifier, by.x = "ID", by.y = "feature")
+
+library(dplyr)
+library(tidyr)
+
+metabolomics_DE_t <- metabolomics_DE_t %>%
+  mutate(HMDB = strsplit(as.character(HMDB), "/")) %>%
+  unnest(HMDB) %>%
+  filter(HMDB != "")
+
+mapping <- metabolomics_DE_t[,c(1,12)]
+
+cosmos_met_input <- metabolomics_DE_t$t
+names(cosmos_met_input) <- metabolomics_DE_t$HMDB
+
+proteomics_DE_t <- proteomics_DE_t %>%
+  mutate(ID = strsplit(as.character(ID), ";")) %>%
+  unnest(ID) %>%
+  filter(ID != "")
+
+cosmos_prot_input <- proteomics_DE_t$t
+names(cosmos_prot_input) <- proteomics_DE_t$ID
+
+
+cosmos_met_input <- cosmos_met_input[which(abs(cosmos_met_input) > 3)]
+cosmos_prot_input <- cosmos_prot_input[which(abs(cosmos_prot_input) > 6)]
+
+cosmos_met_input <- prepare_metab_inputs(cosmos_met_input, c("c","m"))
+
+#In order to adapt options to users specification we can load them into a variable 
+#that will then be passed to preprocess_COSMOS_signaling_to_metabolism CARNIVAL_options parameter
+my_options <- default_CARNIVAL_options(solver = "cplex")
+
+#Here the user should provide a path to its CPLEX executable (only cplex at the moment, other solvers will be documented soon !)
+# my_options$solverPath <- "~/Documents/cplex" #or cbc solver executable
+my_options$solverPath <- "cplex/cplex.exe"
+# my_options$solverPath <- "cbc/cbc-osx/cbc" #or cbc solver executable
+my_options$solver <- "cplex" #or cbc
+# my_options$solver <- "cbc"
+my_options$timelimit <- 1800
+my_options$mipGAP <- 0.05
+my_options$threads <- 6
+
+metab_input <- cosmosR:::filter_input_nodes_not_in_pkn(cosmos_met_input, meta_network)
+sig_input <- cosmosR:::filter_input_nodes_not_in_pkn(cosmos_prot_input, meta_network)
+
+test_for <- preprocess_COSMOS_signaling_to_metabolism(meta_network = meta_network,
+                                                      signaling_data = sig_input,
+                                                      metabolic_data = metab_input,
+                                                      maximum_network_depth = 4,
+                                                      CARNIVAL_options = my_options)
+
+my_options$timelimit <- 1200
+
+test_result_for <- run_COSMOS_signaling_to_metabolism(data = test_for,
+                                                      CARNIVAL_options = my_options)
+
+formatted_res <- format_COSMOS_res(test_result_for)
+
+SIF <- formatted_res[[1]]
+ATT <- formatted_res[[2]]
+
+SIF <- SIF[which(SIF$Weight != 0),]
+
+ATT <- merge(ATT, proteomics_DE_t[,c(3,6)], all.x = T, by.x = "Nodes", by.y = "ID")
+ATT$Nodes <- gsub(",","_",ATT$Nodes)
+
+write_csv(SIF, file = paste("results/adjusted_run/", "SIF.csv", sep = ""))
+write_csv(ATT, file = paste("results/adjusted_run/", "ATT.csv", sep = ""))
+
+
+# include backwards run
+
+my_options$timelimit <- 1800
+
+
+test_back <- preprocess_COSMOS_metabolism_to_signaling(meta_network = meta_network,
+                                                       signaling_data = sig_input,
+                                                       metabolic_data = metab_input,
+                                                       maximum_network_depth = 4,
+                                                       CARNIVAL_options = my_options)
+
+my_options$timelimit <- 14400
+
+test_result_back <- run_COSMOS_metabolism_to_signaling(data = test_back,
+                                                       CARNIVAL_options = my_options)
+
+formatted_res_back <- format_COSMOS_res(test_result_back)
+
+SIF_back <- formatted_res_back[[1]]
+ATT_back <- formatted_res_back[[2]]
+
+SIF_back <- SIF_back[which(SIF_back$Weight != 0),]
+
+
+ATT_back <- merge(ATT_back, proteomics_DE_t[,c(3,6)], all.x = T, by.x = "Nodes", by.y = "ID")
+ATT_back$Nodes <- gsub(",","_",ATT_back$Nodes)
+
+
+write_csv(SIF_back, file = paste("results/adjusted_run/",paste("SIF_back.csv",sep = ""), sep = ""))
+write_csv(ATT_back, file = paste("results/adjusted_run/",paste("ATT_back.csv",sep = ""), sep = ""))
+
+SIF_full <- as.data.frame(rbind(SIF,SIF_back))
+SIF_full <- unique(SIF_full)
+
+ATT_full <- as.data.frame(rbind(ATT,ATT_back))
+ATT_full <- unique(ATT_full)
+
+
+ATT_full <- as.data.frame(ATT_full)
+
+write_csv(SIF_full, file = paste("results/adjusted_run/",paste("SIF_full.csv",sep = ""), sep = ""))
+write_csv(ATT_full, file = paste("results/adjusted_run/",paste("ATT_full.csv",sep = ""), sep = ""))
+
+
